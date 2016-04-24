23f8729b156a383e0bc71ca5abf2152245d4a683
Megan Tiu <megantiu@me.com>
2016-04-24 01:29:37 -0400

    Updating front end copy, user pages, favicon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def store_source
+    session[:get_started] = nil if session[:get_started]
+    session[:get_started] = true if request.referrer == ('/')
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :phone_number) }
+
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :phone_number) }
+  end
 end
